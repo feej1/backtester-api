b2a40f47b7f42c4db6b1eb4a46e22fafeefb91f9
Joseph Fee <josephfee@Josephs-MacBook-Pro.local>
2025-02-11 20:23:30 -0600

    Updated portfolio value unix timestamps to be in milli seconds

--- BackTesterCore/src/Models/Converter/DatetimeConverter.cs
+++ BackTesterCore/src/Models/Converter/DatetimeConverter.cs
@@ -7,7 +7,8 @@ namespace Backtesting.Models
         {
             long unixTimestamp = date.Ticks - new DateTime(1970, 1, 1).Ticks;
             unixTimestamp /= TimeSpan.TicksPerSecond;
-            return unixTimestamp;
+            // need to multiply by 1000 to get milliseconds, required for moment js library on FE
+            return 1000 * unixTimestamp;
         }
     }
 
